f325334dcbefd0922111ba1404d877e36b29bac3
Michael Vandeberg <mvandeberg@users.noreply.github.com>
2020-07-22 16:17:35 -0700

    Update submit API to utilize opaque type

--- libraries/pack/src/submit.hpp
+++ libraries/pack/src/submit.hpp
@@ -1,5 +1,31 @@
 namespace koinos { namespace types { namespace rpc {
 
+struct reserved_query_params {};
+
+struct get_head_info_params {};
+
+typedef std::variant<
+   reserved_query_params,
+   get_head_info_params > query_param_item;
+
+typedef opaque< query_param_item > query_submission;
+
+struct reserved_query_result {};
+
+struct query_error
+{
+   variable_blob error_text;
+};
+
+typedef types::system::head_info get_head_info_result;
+
+typedef std::variant<
+   reserved_query_result,
+   query_error,
+   get_head_info_result > query_item_result;
+
+typedef opaque< query_item_result > query_submission_result;
+
 // Every block has a cryptographic ID.
 // Check the claimed ID against the block content.
 
@@ -38,32 +64,17 @@ struct transaction_submission
    types::variable_blob                       passive_bytes;
 };
 
-struct query_submission
-{
-   types::variable_blob                       query;
-};
-
 typedef std::variant<
    reserved_submission,
    block_submission,
    transaction_submission,
    query_submission > submission_item;
 
-struct get_head_info_params {};
-
-typedef std::variant<
-   get_head_info_params > query_param_item;
-
 struct reserved_submission_result {};
 
 struct block_submission_result {};
 
 struct transaction_submission_result {};
-
-struct query_submission_result
-{
-   types::variable_blob result;
-};
 
 struct submission_error_result
 {
@@ -76,13 +87,5 @@ typedef std::variant<
    transaction_submission_result,
    query_submission_result,
    submission_error_result > submission_result;
-
-typedef query_submission_result query_error;
-
-typedef types::system::head_info get_head_info_result;
-
-typedef std::variant<
-   query_error,
-   get_head_info_result > query_item_result;
 
 } } } // koinos::types::rpc
