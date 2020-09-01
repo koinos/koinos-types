7e33ce194e97eed62074e2062e9ac6c7102f9b9b
Nathaniel Caldwell <nbcwell@gmail.com>
2020-09-01 22:20:47 +0000

    Add several basetypes Stub multihash type "operators"

--- programs/koinos-types/lang/koinos_codegen_golang/rt/basetypes.go
+++ programs/koinos-types/lang/koinos_codegen_golang/rt/basetypes.go
@@ -1,4 +1,37 @@
 
 // TODO: STUBS
-type int128  int64
-type uint128 uint64
+//type int128  int64
+//type uint128 uint64
+
+type boolean bool
+type variable_blob []byte
+type timestamp_type uint64
+type block_height_type uint64
+
+// ----------------
+//  Multihash
+// ----------------
+type multihash struct {
+    id uint64
+    digest variable_blob
+}
+
+func (m0 *multihash) eq(m1 *multihash) boolean {
+    return false
+}
+
+func (m0 *multihash) lt(m1 *multihash) boolean {
+    return false
+}
+
+func (m0 *multihash) gt(m1 *multihash) boolean {
+    return false
+}
+
+// ----------------
+//  Multihash Vector
+// ----------------
+type multihash_vector struct {
+    id uint64
+    digests []variable_blob
+}
