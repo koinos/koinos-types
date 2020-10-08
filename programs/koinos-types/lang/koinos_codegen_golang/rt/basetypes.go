a782c2c0c97ecef90d347801c35bd78d2d69af70
Nathaniel Caldwell <nbcwell@gmail.com>
2020-10-08 20:42:29 +0000

    Remove custom append

--- programs/koinos-types/lang/koinos_codegen_golang/rt/basetypes.go
+++ programs/koinos-types/lang/koinos_codegen_golang/rt/basetypes.go
@@ -20,7 +20,7 @@ func (n Boolean) Serialize(vb VariableBlob) VariableBlob {
     if n {
         b = 1
     }
-    return AppendToVBlob(vb, b)
+    return append(vb, b)
 }
 
 func DeserializeBoolean(vb VariableBlob) (uint64,Boolean) {
@@ -38,7 +38,7 @@ func DeserializeBoolean(vb VariableBlob) (uint64,Boolean) {
 type Int8 int8
 
 func (n Int8) Serialize(vb VariableBlob) VariableBlob {
-    return AppendToVBlob(vb, byte(n))
+    return append(vb, byte(n))
 }
 
 func DeserializeInt8(vb VariableBlob) (uint64,Int8) {
@@ -52,7 +52,7 @@ func DeserializeInt8(vb VariableBlob) (uint64,Int8) {
 type UInt8 uint8
 
 func (n UInt8) Serialize(vb VariableBlob) VariableBlob {
-    return AppendToVBlob(vb, byte(n))
+    return append(vb, byte(n))
 }
 
 func DeserializeUint8(vb VariableBlob) (uint64,UInt8) {
@@ -68,7 +68,7 @@ type Int16 int16
 func (n Int16) Serialize(vb VariableBlob) VariableBlob {
     b := make([]byte, 2)
     binary.BigEndian.PutUint16(b, uint16(n))
-    return AppendToVBlob(vb, b...)
+    return append(vb, b...)
 }
 
 func DeserializeInt16(vb VariableBlob) (uint64,Int16) {
@@ -84,7 +84,7 @@ type UInt16 uint16
 func (n UInt16) Serialize(vb VariableBlob) VariableBlob {
     b := make([]byte, 2)
     binary.BigEndian.PutUint16(b, uint16(n))
-    return AppendToVBlob(vb, b...)
+    return append(vb, b...)
 }
 
 func DeserializeUInt16(vb VariableBlob) (uint64,UInt16) {
@@ -101,7 +101,7 @@ type Int32 int32
 func (n Int32) Serialize(vb VariableBlob) VariableBlob {
     b := make([]byte, 4)
     binary.BigEndian.PutUint32(b, uint32(n))
-    return AppendToVBlob(vb, b...)
+    return append(vb, b...)
 }
 
 func DeserializeInt32(vb VariableBlob) (uint64,Int32) {
@@ -117,7 +117,7 @@ type UInt32 uint32
 func (n UInt32) Serialize(vb VariableBlob) VariableBlob {
     b := make([]byte, 4)
     binary.BigEndian.PutUint32(b, uint32(n))
-    return AppendToVBlob(vb, b...)
+    return append(vb, b...)
 }
 
 func DeserializeUInt32(vb VariableBlob) (uint64,UInt32) {
@@ -133,7 +133,7 @@ type Int64 int64
 func (n Int64) Serialize(vb VariableBlob) VariableBlob {
     b := make([]byte, 8)
     binary.BigEndian.PutUint64(b, uint64(n))
-    return AppendToVBlob(vb, b...)
+    return append(vb, b...)
 }
 
 func DeserializeInt64(vb VariableBlob) (uint64,Int64) {
@@ -149,7 +149,7 @@ type UInt64 uint64
 func (n UInt64) Serialize(vb VariableBlob) VariableBlob {
     b := make([]byte, 8)
     binary.BigEndian.PutUint64(b, uint64(n))
-    return AppendToVBlob(vb, b...)
+    return append(vb, b...)
 }
 
 func DeserializeUInt64(vb VariableBlob) (uint64,UInt64) {
@@ -189,14 +189,14 @@ type VariableBlob []byte
 func (n VariableBlob) Serialize(vb VariableBlob) VariableBlob {
     header := make([]byte, binary.MaxVarintLen64)
     bytes := binary.PutUvarint(header, uint64(len(n)))
-    vb = AppendToVBlob(vb, header[:bytes]...)
-    return AppendToVBlob(vb, n...)
+    vb = append(vb, header[:bytes]...)
+    return append(vb, n...)
 }
 
 func DeserializeVariableBlob(vb VariableBlob) (uint64,VariableBlob) {
     size,bytes := binary.Uvarint(vb)
     var result VariableBlob = VariableBlob(make([]byte, 0, size))
-    return uint64(uint64(bytes)+size), AppendToVBlob(result, vb[bytes:]...)
+    return uint64(uint64(bytes)+size), append(result, vb[bytes:]...)
 }
 
 // --------------------------------
@@ -208,7 +208,7 @@ type TimestampType uint64
 func (n TimestampType) Serialize(vb VariableBlob) VariableBlob {
     b := make([]byte, 8)
     binary.BigEndian.PutUint64(b, uint64(n))
-    return AppendToVBlob(vb, b...)
+    return append(vb, b...)
 }
 
 func DeserializeTimestampType(vb VariableBlob) (uint32,TimestampType) {
@@ -224,7 +224,7 @@ type BlockHeightType uint64
 func (n BlockHeightType) Serialize(vb VariableBlob) VariableBlob {
     b := make([]byte, 8)
     binary.BigEndian.PutUint64(b, uint64(n))
-    return AppendToVBlob(vb, b...)
+    return append(vb, b...)
 }
 
 func DeserializeBlockHeightType(vb VariableBlob) (uint32,BlockHeightType) {
@@ -259,22 +259,4 @@ func (m0 *Multihash) gt(m1 *Multihash) Boolean {
 type MultihashVector struct {
     Id UInt64
     Digests []VariableBlob
-}
-
-// --------------------------------
-//  Utility functions
-// --------------------------------
-
-func AppendToVBlob(vblob VariableBlob, data ...byte) []byte {
-    m := len(vblob)
-    n := m + len(data)
-    if n > cap(vblob) { // if necessary, reallocate
-        // allocate double what's needed
-        new_vblob := make([]byte, (n+1)*2)
-        copy(new_vblob, vblob)
-        vblob = new_vblob
-    }
-    vblob = vblob[0:n]
-    copy(vblob[m:n], data)
-    return vblob
 }
