b6fab33c9668e64d1ca53316d58b824ccd98e3a1
Steve Gerbino <steve@gerbino.co>
2020-11-06 17:45:13 -0500

    #62: Change pushd and popd locations in test file

--- ci/test.sh
+++ ci/test.sh
@@ -5,15 +5,13 @@ set -x
 
 if [ "$RUN_TYPE" = "test" ]; then
    # C++ tests
-   pushd ../build/tests/cpp
+   pushd build/tests/cpp
    ctest -j3 --output-on-failure
    popd
 
    # Golang tests
-   pushd ..
    GOPATH=$GOPATH:$(pwd)/build/generated/golang
    go get -d ./tests/golang
    go test ./tests/golang
-   popd
 fi
 
