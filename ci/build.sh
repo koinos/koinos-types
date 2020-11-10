f28edbab72ab96a86a9cf6e15f11ac49f35f8bf3
Steve Gerbino <steve@gerbino.co>
2020-11-10 01:44:30 -0500

    #76: Add golang code coverage, convert to lcov style, merge results with cpp coverage, upload merged file to coveralls

--- ci/build.sh
+++ ci/build.sh
@@ -4,7 +4,7 @@ set -e
 set -x
 
 mkdir build
-cd build
+pushd build
 
 if [ "$RUN_TYPE" = "test" ]; then
    if [ "$TRAVIS_OS_NAME" = "osx" ]; then
@@ -16,5 +16,12 @@ if [ "$RUN_TYPE" = "test" ]; then
    fi
 elif [ "$RUN_TYPE" = "coverage" ]; then
    cmake -DCMAKE_BUILD_TYPE=Debug -DCOVERAGE=ON ..
-   cmake --build . --config Debug --parallel 3 --target coverage
+   cmake --build . --config Debug --parallel 3 --target coverage --target golang
+
+   popd
+   go test ./tests/golang -coverprofile=./build/go-coverage.out -coverpkg=./build/generated/golang/src/koinos
+   gcov2lcov -infile=./build/go-coverage.out -outfile=./build/go-coverage.info
+
+   lcov -a ./build/coverage.info -a ./build/go-coverage.info -o ./build/merged.info
 fi
+
