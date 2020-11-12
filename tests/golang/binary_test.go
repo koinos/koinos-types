23b2c9db1db6cfd23bc486cf412d530f760dab9d
Steve Gerbino <steve@gerbino.co>
2020-11-12 16:27:48 -0500

    #53: Implemented integer base type tests in golang

--- tests/golang/binary_test.go
+++ tests/golang/binary_test.go
@@ -50,7 +50,199 @@ func TestBoolean(t *testing.T) {
    }
 }
 
-func TestBasic(t *testing.T) {
+func TestInt8(t *testing.T) {
+   integer := Int8(-128)
+   expected := []byte{0x80}
+   result := NewVariableBlob()
+   result = integer.Serialize(result)
+   if !bytes.Equal(*result, expected) {
+      t.Errorf("*result != expected")
+   }
+   size, integer2, err := DeserializeInt8(result)
+   if err != nil {
+      t.Errorf("err != nil (%s)", err)
+   }
+   if *integer2 != Int8(-128) {
+      t.Errorf("*integer2 != Int8(-128) (%d != %d)", *integer2, Int8(-128))
+   }
+   if size != 1 {
+      t.Errorf("size != 1 (%d != 1)", size)
+   }
+
+   result = NewVariableBlob()
+   result = integer2.Serialize(result)
+   if !bytes.Equal(*result, expected) {
+      t.Errorf("*result != expected (%d != %d)", *result, expected)
+   }
+
+   vb := VariableBlob{}
+   _, _, err = DeserializeInt8(&vb)
+   if err == nil {
+      t.Errorf("err == nil")
+   }
+}
+
+func TestUInt8(t *testing.T) {
+   integer := UInt8(255)
+   expected := []byte{0xFF}
+   result := NewVariableBlob()
+   result = integer.Serialize(result)
+   if !bytes.Equal(*result, expected) {
+      t.Errorf("*result != expected")
+   }
+   size, integer2, err := DeserializeUInt8(result)
+   if err != nil {
+      t.Errorf("err != nil (%s)", err)
+   }
+   if *integer2 != UInt8(255) {
+      t.Errorf("*integer2 != UInt8(255) (%d != %d)", *integer2, UInt8(255))
+   }
+   if size != 1 {
+      t.Errorf("size != 1 (%d != 1)", size)
+   }
+
+   result = NewVariableBlob()
+   result = integer2.Serialize(result)
+   if !bytes.Equal(*result, expected) {
+      t.Errorf("*result != expected (%d != %d)", *result, expected)
+   }
+
+   vb := VariableBlob{}
+   _, _, err = DeserializeUInt8(&vb)
+   if err == nil {
+      t.Errorf("err == nil")
+   }
+}
+
+func TestInt16(t *testing.T) {
+   integer := Int16(-32768)
+   expected := []byte{0x80, 0x00}
+   result := NewVariableBlob()
+   result = integer.Serialize(result)
+   if !bytes.Equal(*result, expected) {
+      t.Errorf("*result != expected")
+   }
+   size, integer2, err := DeserializeInt16(result)
+   if err != nil {
+      t.Errorf("err != nil (%s)", err)
+   }
+   if *integer2 != Int16(-32768) {
+      t.Errorf("*integer2 != Int16(-32768) (%d != %d)", *integer2, Int16(-32768))
+   }
+   if size != 2 {
+      t.Errorf("size != 2 (%d != 2)", size)
+   }
+
+   result = NewVariableBlob()
+   result = integer2.Serialize(result)
+   if !bytes.Equal(*result, expected) {
+      t.Errorf("*result != expected (%d != %d)", *result, expected)
+   }
+
+   vb := VariableBlob{0x08}
+   _, _, err = DeserializeInt16(&vb)
+   if err == nil {
+      t.Errorf("err == nil")
+   }
+}
+
+func TestUInt16(t *testing.T) {
+   integer := UInt16(65535)
+   expected := []byte{0xFF, 0xFF}
+   result := NewVariableBlob()
+   result = integer.Serialize(result)
+   if !bytes.Equal(*result, expected) {
+      t.Errorf("*result != expected")
+   }
+   size, integer2, err := DeserializeUInt16(result)
+   if err != nil {
+      t.Errorf("err != nil (%s)", err)
+   }
+   if *integer2 != UInt16(65535) {
+      t.Errorf("*integer2 != UInt32(65535) (%d != %d)", *integer2, UInt16(65535))
+   }
+   if size != 2 {
+      t.Errorf("size != 2 (%d != 2)", size)
+   }
+
+   result = NewVariableBlob()
+   result = integer2.Serialize(result)
+   if !bytes.Equal(*result, expected) {
+      t.Errorf("*result != expected (%d != %d)", *result, expected)
+   }
+
+   vb := VariableBlob{0xFF}
+   _, _, err = DeserializeUInt16(&vb)
+   if err == nil {
+      t.Errorf("err == nil")
+   }
+}
+
+func TestInt32(t *testing.T) {
+   integer := Int32(-2147483648)
+   expected := []byte{0x80, 0x00, 0x00, 0x00}
+   result := NewVariableBlob()
+   result = integer.Serialize(result)
+   if !bytes.Equal(*result, expected) {
+      t.Errorf("*result != expected")
+   }
+   size, integer2, err := DeserializeInt32(result)
+   if err != nil {
+      t.Errorf("err != nil (%s)", err)
+   }
+   if *integer2 != Int32(-2147483648) {
+      t.Errorf("*integer2 != Int32(-2147483648) (%d != %d)", *integer2, Int32(-2147483648))
+   }
+   if size != 4 {
+      t.Errorf("size != 4 (%d != 4)", size)
+   }
+
+   result = NewVariableBlob()
+   result = integer2.Serialize(result)
+   if !bytes.Equal(*result, expected) {
+      t.Errorf("*result != expected (%d != %d)", *result, expected)
+   }
+
+   vb := VariableBlob{0x08, 0x00, 0x00}
+   _, _, err = DeserializeInt32(&vb)
+   if err == nil {
+      t.Errorf("err == nil")
+   }
+}
+
+func TestUInt32(t *testing.T) {
+   integer := UInt32(4294967295)
+   expected := []byte{0xFF, 0xFF, 0xFF, 0xFF}
+   result := NewVariableBlob()
+   result = integer.Serialize(result)
+   if !bytes.Equal(*result, expected) {
+      t.Errorf("*result != expected")
+   }
+   size, integer2, err := DeserializeUInt32(result)
+   if err != nil {
+      t.Errorf("err != nil (%s)", err)
+   }
+   if *integer2 != UInt32(4294967295) {
+      t.Errorf("*integer2 != UInt32(4294967295) (%d != %d)", *integer2, UInt32(4294967295))
+   }
+   if size != 4 {
+      t.Errorf("size != 4 (%d != 4)", size)
+   }
+
+   result = NewVariableBlob()
+   result = integer2.Serialize(result)
+   if !bytes.Equal(*result, expected) {
+      t.Errorf("*result != expected (%d != %d)", *result, expected)
+   }
+
+   vb := VariableBlob{0xFF, 0xFF, 0xFF}
+   _, _, err = DeserializeUInt32(&vb)
+   if err == nil {
+      t.Errorf("err == nil")
+   }
+}
+
+func TestInt64(t *testing.T) {
    integer := Int64(-256)
    expected := []byte{0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x00}
    result := NewVariableBlob()
@@ -77,6 +269,77 @@ func TestBasic(t *testing.T) {
 
    vb := VariableBlob{0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF}
    _, _, err = DeserializeInt64(&vb)
+   if err == nil {
+      t.Errorf("err == nil")
+   }
+}
+
+func TestUInt64(t *testing.T) {
+   integer := UInt64(18446744073709551615)
+   expected := []byte{0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF}
+   result := NewVariableBlob()
+   result = integer.Serialize(result)
+   if !bytes.Equal(*result, expected) {
+      t.Errorf("*result != expected")
+   }
+   size, integer2, err := DeserializeUInt64(result)
+   if err != nil {
+      t.Errorf("err != nil (%s)", err)
+   }
+   if *integer2 != UInt64(18446744073709551615) {
+      t.Errorf("*integer2 != UInt64(18446744073709551615) (%d != %d)", *integer2, UInt64(18446744073709551615))
+   }
+   if size != 8 {
+      t.Errorf("size != 8 (%d != 8)", size)
+   }
+
+   result = NewVariableBlob()
+   result = integer2.Serialize(result)
+   if !bytes.Equal(*result, expected) {
+      t.Errorf("*result != expected (%d != %d)", *result, expected)
+   }
+
+   vb := VariableBlob{0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF}
+   _, _, err = DeserializeUInt64(&vb)
+   if err == nil {
+      t.Errorf("err == nil")
+   }
+}
+
+func TestInt128(t *testing.T) {
+   integer := NewInt128("-170141183460469231731687303715884105728")
+   expected := []byte{
+      0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+      0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+   }
+   result := NewVariableBlob()
+   result = integer.Serialize(result)
+   if !bytes.Equal(*result, expected) {
+      t.Errorf("*result != expected (%d)", result)
+   }
+   size, integer2, err := DeserializeInt128(result)
+   if err != nil {
+      t.Errorf("err != nil (%s)", err)
+   }
+
+   if NewInt128("-170141183460469231731687303715884105728").Value.Cmp(&integer2.Value) != 0 {
+      t.Errorf("*integer2 != Int128(-170141183460469231731687303715884105728) (%s != %s)", (*integer2).Value.String(), NewInt128("-170141183460469231731687303715884105728").Value.String())
+   }
+   if size != 16 {
+      t.Errorf("size != 16 (%d != 16)", size)
+   }
+
+   result = NewVariableBlob()
+   result = integer2.Serialize(result)
+   if !bytes.Equal(*result, expected) {
+      t.Errorf("*result != expected (%d != %d)", *result, expected)
+   }
+
+   vb := VariableBlob{
+      0x80, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+      0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+   }
+   _, _, err = DeserializeInt128(&vb)
    if err == nil {
       t.Errorf("err == nil")
    }
@@ -114,10 +377,44 @@ func TestUInt128(t *testing.T) {
    }
 }
 
-func TestUInt160(t *testing.T) {
-   to_bin := NewUInt160("680564733841876926926749214863536422911")
+func TestInt160(t *testing.T) {
+   toBin := NewInt160("-730750818665451459101842416358141509827966271488")
    result := NewVariableBlob()
-   result = to_bin.Serialize(result)
+   result = toBin.Serialize(result)
+
+   expected := []byte{
+      0x80, 0x00, 0x00, 0x00,
+      0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+      0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+   }
+
+   if !bytes.Equal(*result, expected) {
+      t.Errorf("*result != expected")
+   }
+
+   _, fromBin, err := DeserializeInt160(result)
+   if err != nil {
+      t.Errorf("err != nil")
+   }
+   if toBin.Value.Cmp(&fromBin.Value) != 0 {
+      t.Errorf("to_bin != from_bin")
+   }
+
+   vb := VariableBlob{
+      0x00, 0x00, 0x00, 0x01,
+      0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+      0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+   }
+   _, _, err = DeserializeInt160(&vb)
+   if err == nil {
+      t.Errorf("err == nil")
+   }
+}
+
+func TestUInt160(t *testing.T) {
+   toBin := NewUInt160("680564733841876926926749214863536422911")
+   result := NewVariableBlob()
+   result = toBin.Serialize(result)
 
    expected := []byte{
       0x00, 0x00, 0x00, 0x01,
@@ -129,12 +426,12 @@ func TestUInt160(t *testing.T) {
       t.Errorf("*result != expected")
    }
 
-   _, from_bin, err := DeserializeUInt160(result)
+   _, fromBin, err := DeserializeUInt160(result)
    if err != nil {
       t.Errorf("err != nil")
    }
-   if to_bin.Value.Cmp(&from_bin.Value) != 0 {
-      t.Errorf("to_bin != from_bin")
+   if toBin.Value.Cmp(&fromBin.Value) != 0 {
+      t.Errorf("toBin != fromBin")
    }
 
    vb := VariableBlob{
@@ -143,6 +440,42 @@ func TestUInt160(t *testing.T) {
       0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
    }
    _, _, err = DeserializeUInt160(&vb)
+   if err == nil {
+      t.Errorf("err == nil")
+   }
+}
+
+func TestInt256(t *testing.T) {
+   toBin := NewInt256("-57896044618658097711785492504343953926634992332820282019728792003956564819968")
+   result := NewVariableBlob()
+   result = toBin.Serialize(result)
+
+   expected := []byte{
+      0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+      0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+      0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+      0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+   }
+
+   if !bytes.Equal(*result, expected) {
+      t.Errorf("*result != expected")
+   }
+
+   _, fromBin, err := DeserializeInt256(result)
+   if err != nil {
+      t.Errorf("err != nil")
+   }
+   if toBin.Value.Cmp(&fromBin.Value) != 0 {
+      t.Errorf("toBin != fromBin (%s != %s)", toBin.Value.String(), fromBin.Value.String())
+   }
+
+   vb := VariableBlob{
+      0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+      0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01,
+      0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+      0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+   }
+   _, _, err = DeserializeInt256(&vb)
    if err == nil {
       t.Errorf("err == nil")
    }
