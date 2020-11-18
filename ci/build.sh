cf4111d85be06831ab2872aa52d3d3c12cff1c54
Michael Vandeberg <mvandeberg@users.noreply.github.com>
2020-11-17 16:43:18 -0800

    Fix typo

--- ci/build.sh
+++ ci/build.sh
@@ -23,7 +23,7 @@ elif [ "$RUN_TYPE" = "coverage" ]; then
    go test ./tests/golang -coverprofile=./build/go-coverage.out -coverpkg=./build/generated/golang/src/koinos
    go test ./build/generated/golang/tests -coverprofile=./build/go-generated-coverage.out -coverpkg=./build/generated/golang/src/koinos
    gcov2lcov -infile=./build/go-coverage.out -outfile=./build/go-coverage.info
-   gcov2lcov -infile=./build/go-generated-coverage.out -outfile=./build/go-generaged-coverage.info
+   gcov2lcov -infile=./build/go-generated-coverage.out -outfile=./build/go-generated-coverage.info
 
    lcov -a ./build/coverage.info -a ./build/go-coverage.info -o ./build/merged.info
    lcov -a ./build/merged.info -a ./build/go-generated-coverage.info -o ./build/merged.info
