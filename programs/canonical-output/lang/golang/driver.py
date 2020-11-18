6c7c27abfa0bed5ba9583e4ea772b43d658c5b5a
Steve Gerbino <steve@gerbino.co>
2020-11-18 17:37:48 -0500

    #53: Do not override GOPATH variables when executing the golang driver program

--- programs/canonical-output/lang/golang/driver.py
+++ programs/canonical-output/lang/golang/driver.py
@@ -4,9 +4,7 @@ import os
 import shutil
 import subprocess
 
-gopath = os.popen('go env GOPATH').read()[:-1] + ":" + os.path.join(os.getcwd(), "../../../../generated/golang")
-gocache = os.popen('go env GOCACHE').read()
 go_cmd = shutil.which('go') + ' run test.go'
 
-p = subprocess.Popen([go_cmd], env={"GOPATH": gopath, "GOCACHE": gocache, "HOME": os.environ["HOME"]}, shell=True)
+p = subprocess.Popen([go_cmd], shell=True)
 p.wait()
