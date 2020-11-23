ace1811d52c9f116d085f864b5160b07270fff01
Michael Vandeberg <mvandeberg@users.noreply.github.com>
2020-11-23 11:04:56 -0800

    CI without Mac OS #95

--- ci/build.sh
+++ ci/build.sh
@@ -7,15 +7,10 @@ mkdir build
 pushd build
 
 if [ "$RUN_TYPE" = "test" ]; then
-   if [ "$TRAVIS_OS_NAME" = "osx" ]; then
-      cmake -DCMAKE_BUILD_TYPE=Release -GXcode -DPYTHON_BINARY=/usr/local/bin/python3 ..
-      cmake --build . --config Release --parallel 3
-   else
-      cmake -DCMAKE_BUILD_TYPE=Release ..
-      cmake --build . --config Release --parallel 3
-   fi
+   cmake -DCMAKE_BUILD_TYPE=Release ..
+   cmake --build . --config Release --parallel 3
 elif [ "$RUN_TYPE" = "coverage" ]; then
-   cmake -DCMAKE_BUILD_TYPE=Debug -DCOVERAGE=ON -DPYTHON_BINARY=/usr/local/bin/python3 ..
+   cmake -DCMAKE_BUILD_TYPE=Debug -DCOVERAGE=ON ..
    cmake --build . --config Debug --parallel 3 --target coverage --target golang
 
    popd
--- ci/install.sh
+++ ci/install.sh
@@ -1,35 +1,21 @@
 #!/bin/bash
 
-if [ "$TRAVIS_OS_NAME" = "linux" ]; then
-   sudo apt-get install -y \
-      libboost-all-dev \
-      python3 \
-      python3-pip \
-      python3-setuptools \
-      valgrind \
-      ccache
+if [ "$CC" = "clang-11" ]; then
+   wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add -
+   sudo apt-add-repository 'deb http://apt.llvm.org/focal/ llvm-toolchain-focal-11 main'
+   sudo apt-get install -y llvm-11 clang-11
+fi
 
-   eval "$(gimme 1.15.4)"
-   source ~/.gimme/envs/go1.15.4.env
+eval "$(gimme 1.15.4)"
+source ~/.gimme/envs/go1.15.4.env
 
-   export GOPATH=~/go:$(pwd)/build/generated/golang
-   export PATH=$PATH:~/go/bin
+export GOPATH=~/go:$(pwd)/build/generated/golang
+export PATH=$PATH:~/go/bin
 
-elif [ "$TRAVIS_OS_NAME" = "osx" ]; then
-   brew install cmake \
-      boost \
-      go \
-      ccache
-
-   export GOPATH=~/go:$(pwd)/build/generated/golang
-   export PATH=$PATH:~/go/bin
-
-   if [ "$RUN_TYPE" = "coverage" ]; then
-      brew install lcov
-      sudo gem install coveralls-lcov
-      go get -u github.com/jandelgado/gcov2lcov
-   fi
+if [ "$RUN_TYPE" = "coverage" ]; then
+   sudo apt-get install -y lcov ruby
+   sudo gem install coveralls-lcov
+   go get -u github.com/jandelgado/gcov2lcov
 fi
 
 pip3 install --user dataclasses-json Jinja2 importlib_resources pluginbase
-
