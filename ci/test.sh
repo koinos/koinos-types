4d04dca684be0be5f910cdb6ce46463e88b4f8a9
Michael Vandeberg <mvandeberg@users.noreply.github.com>
2020-11-24 14:02:52 -0800

    Enforce golang linting

--- ci/test.sh
+++ ci/test.sh
@@ -16,4 +16,6 @@ if [ "$RUN_TYPE" = "test" ]; then
 
    # Compare multilingual outputs
    python3 programs/canonical-output/check_canonical_output.py --lang-dir build/programs/canonical-output/lang --test-data programs/koinos-types/json/test_data.json
+
+   golint -set_exit_status ./...
 fi
