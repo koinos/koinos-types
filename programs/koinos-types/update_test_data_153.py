25ccef7e8c2e2d9d1b4714d20a9d76f5d56d3840
theoreticalbts <theoreticalbts@users.noreply.github.com>
2021-03-23 19:34:42 +0000

    Update migration script #153

--- programs/koinos-types/update_test_data_153.py
+++ programs/koinos-types/update_test_data_153.py
@@ -22,9 +22,18 @@ def convert_hook(pairs):
     if set(d.keys()) != set(["hash", "digest"]):
         return d
     id_bytes = encode_varint(d["hash"])
-    digest_bytes = base58.b58decode(d["digest"])
-    digest_len_bytes = encode_varint(len(digest_bytes))
-    result = b"z"+base58.b58encode(id_bytes + digest_len_bytes + digest_bytes)
+    digest_bytes = base58.b58decode(d["digest"][1:])
+    # digest_len_bytes = encode_varint(len(digest_bytes))
+    # digest_len_bytes seems to already be included in the data
+    result = b"z"+base58.b58encode(id_bytes + digest_bytes)
+    if d["digest"] == "zX1ECcx53wEief":
+        print("")
+        print(d)
+        print("id_bytes:", id_bytes.hex())
+        # print("digest_len_bytes:", digest_len_bytes.hex())
+        print("digest_bytes:", digest_bytes.hex())
+        print("concat:", (id_bytes + digest_bytes).hex())
+        print("result:", result)
     return result.decode("ascii")
 
 def main():
