cc660a82c8fcc993ece3ed40e11e479cd9eeb49d
Julian Gonzalez <joticajulian@gmail.com>
2021-04-10 23:41:16 +0000

    typescript: setup jinja

--- programs/koinos-types/build.sh
+++ programs/koinos-types/build.sh
@@ -5,9 +5,9 @@ set -x
 
 rm -Rf build
 mkdir -p build
-python3 -m koinos_reflect.analyze block.bt types.hpp block.hpp -s -o build/block.schema
-python3 -m koinos_codegen.codegen --target-path lang -o build -p koinos_protocol build/block.schema
+python3 -m koinos_reflect.analyze ../../types/basetypes.def ../../types/common.hpp -s -o build/block.schema
+python3 -m koinos_codegen.codegen --target-path lang --target typescript -o build -p koinos_protocol build/block.schema
 
-cp tester.py build
-cd build
-python3 tester.py
+# cp tester.py build
+# cd build
+# python3 tester.py
