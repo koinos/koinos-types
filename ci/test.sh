36cc6e29b7937ce630060c74cd3f5fd374357638
theoretical <theoreticalbts@users.noreply.github.com>
2021-05-26 17:16:21 -0400

    Automatically check ID's were properly generated in CI

--- ci/test.sh
+++ ci/test.sh
@@ -4,6 +4,9 @@ set -e
 set -x
 
 if [ "$RUN_TYPE" = "test" ]; then
+   # Check the ID headers were properly generated
+   python3 programs/generate-ids/codegen_ids.py -t programs/generate-ids/templates -o types -c types/system_call_names.json
+
    # C++ tests
    pushd build/tests/cpp
    ctest -j3 --output-on-failure
